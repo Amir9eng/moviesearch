a9424d6120b6fe6d8552c2ac7acf3b0e52233b75
Amir9eng <amirmukhtarme@gmail.com>
2020-12-22 18:20:56 +0100

    Nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import './NavBar.scss'
+import React from "react";
+import "./NavBar.scss";
 
-export default function Navbar() {
-    return (
-        <div className="flex justify-between bg-blue-900 items-center">
-            <div className="flex items-center justify-center awon">
-            <h1 className= "text-red-500 text-xl mx-8">Movie Spy</h1>
-            <p className="text-white">Movies</p>
-            <p className="text-white mx-4">Tv shows</p>
-            <p className="text-white mx-4">People</p>
-            <p className="text-white mx-4">More</p>
-            </div>
-            <div className="flex">
-                <p className="text-white mx-4">SignIn</p>
-                <p className="text-white mx-4">SignUp</p>
-            </div>
-        </div>
-    )
+export default function NavBar() {
+  return (
+    <div className="flex justify-between bg-blue-900 items-center">
+      <div className="flex items-center justify-center">
+        <h1 className="text-red-500 text-xl mx-8 awon">Movie Spy</h1>
+        <p className="text-white">Movies</p>
+        <p className="text-white mx-4">Tv shows</p>
+        <p className="text-white mx-4">People</p>
+        <p className="text-white mx-4">More</p>
+      </div>
+      <div className="flex">
+        <p className="text-white mx-4">SignIn</p>
+        <p className="text-white mx-4">SignUp</p>
+      </div>
+    </div>
+  );
 }
